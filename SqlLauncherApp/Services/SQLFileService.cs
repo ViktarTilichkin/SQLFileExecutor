edc454665d6f7a5fa821d7ba27eaa73a6ab166f5
ViktarTilichkin <tilichkin@mail.ru>
2023-04-07 23:11:43 +0300

    create folders, create files

---
+++ SqlLauncherApp/Services/SQLFileService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLauncherApp.Services
+{
+    public class SQLFileService : ISQLFileService
+    {
+        private readonly ISQLFileService m_Repository;
+        public SQLFileService(ISQLFileService repository) 
+        {
+            m_Repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+    }
+}
---
+++ SqlLauncherApp/Utilities/AppSettingHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace SqlLauncherApp.Utilities
+{
+    public static class AppSettingHelper
+    {
+        private static string _connetionString;
+        public static string ConnectionString => _connetionString ??= GetBilder().GetConnectionString("MyDatabase");
+
+
+        private static string _pathFolder;
+        public static string PathFolder => _pathFolder ??= GetBilder().GetValue<string>("PathFolder");
+
+        private static IConfigurationRoot GetBilder()
+        {
+            return new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .Build();
+        }
+    }
+}
